91f7e834afaacea2fc13f15f5818e82a609aa9aa
lijiuzhen <lijiuzhen@bytedance.com>
2024-07-07 16:36:19 +0800

    update01

--- src/main/java/org/example/ClassDemo001.java
+++ src/main/java/org/example/ClassDemo001.java
@@ -3,5 +3,6 @@ package org.example;
 public class ClassDemo001 {
     public static void main(String[] args) {
         System.out.println("lijiuzhen");
+        System.out.println("java");
     }
 }
